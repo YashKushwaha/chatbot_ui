a33d86c7c4702abd52d7683e6715e45f580ff0e4
Yash Kushwaha <yash1158@gmail.com>
2025-06-20 18:20:28 +0530

    structure for back and front end created. fastapi serves simple webpage

---
+++ back_end/main.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+from fastapi import FastAPI
+from fastapi import Request
+
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+from config_settings import *
+
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+app.mount("/images", StaticFiles(directory=IMAGES_DIR), name="images")
+
+
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "chat_endpoint": "/chat"
+    })
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    app_path = Path(__file__).resolve().with_suffix('').name  # gets filename without .py
+    uvicorn.run(f"{app_path}:app", host="localhost", port=8000, reload=True)
